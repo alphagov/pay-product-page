301bd4723616fa9fec166e0398c62f3bc373180f
Jon Heslop <jon@jonheslop.com>
2018-01-19 17:03:41 +0000

    Fix broken JS

--- source/pay-product-page/javascripts/header.js
+++ source/pay-product-page/javascripts/header.js
@@ -1,21 +1,23 @@
 (function(){
-  function addEventListener(el, eventName, handler) {
-    if (el.addEventListener) {
-      el.addEventListener(eventName, handler);
-    } else {
-      el.attachEvent('on' + eventName, function(){
-        handler.call(el);
-      });
+  document.addEventListener("DOMContentLoaded", function(event) { 
+    function addEventListener(el, eventName, handler) {
+      if (el.addEventListener) {
+        el.addEventListener(eventName, handler);
+      } else {
+        el.attachEvent('on' + eventName, function(){
+          handler.call(el);
+        });
+      }
     }
-  }
 
-  var navToggle = document.getElementById('show-menu');
-  var nav = document.getElementById('navigation');
+    var navToggle = document.getElementById('show-menu');
+    var nav = document.getElementById('navigation');
 
-  addEventListener(navToggle, 'change', function () {
-    var navIsVisible = navToggle.checked;
+    addEventListener(navToggle, 'change', function () {
+      var navIsVisible = navToggle.checked;
 
-    navToggle.setAttribute('aria-expanded', (navIsVisible ? 'true' : 'false'));
-    nav.setAttribute('aria-hidden', (navIsVisible ? 'false' : 'true'));
-  });  
+      navToggle.setAttribute('aria-expanded', (navIsVisible ? 'true' : 'false'));
+      nav.setAttribute('aria-hidden', (navIsVisible ? 'false' : 'true'));
+    });
+  });
 }());

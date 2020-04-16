7f4cc31664bc1567e5a9ae12c1c3c6931fdcf26d
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2020-04-16 10:31:16 +0100

    PP-6274: Add cookie banner
    This commit:
    - Adds the cookie banner
    - Sets the analytics cookie policy ("gov_pay_cookies_policy") with value
      `{"analytics":true|false}` depending on user selection of Yes or No on the
    cookie banner. The cookie expires a year into the future.
    
    Code in this commit was lifted from
    https://github.com/alphagov/digitalmarketplace-govuk-frontend/blob/5b8a9003877432d650f8d965c6c5f0f4886127f2/src/digitalmarketplace/components/cookie-banner/cookie-banner.js
    and
    https://github.com/alphagov/digitalmarketplace-govuk-frontend/blob/5b8a9003877432d650f8d965c6c5f0f4886127f2/src/digitalmarketplace/helpers/cookie/cookie-functions.js
    into cookie-banner.js.

--- source/pay-product-page/javascripts/application.js
+++ source/pay-product-page/javascripts/application.js
@@ -1,8 +1,18 @@
 //= require govuk/all.js
 //= require gaap-analytics.min.js
 //= require cookies.js
+//= require cookie-banner.js
 
 document.addEventListener("DOMContentLoaded", function(event) {
   GOVUKFrontend.initAll()
   GAAP.analytics.eventTracking.init()
+  initCookieBanner();
 });
+
+function initCookieBanner() {
+  const $cookieBanner = document.querySelector('[data-module="govuk-pay-cookie-banner"]');
+  if ($cookieBanner) {
+    new CookieBanner($cookieBanner).init();
+  }
+}
+

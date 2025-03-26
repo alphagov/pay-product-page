6c617a78624cbf71f8eba2570427d9a7ea10d728
Marco Tranchino <marco.tranchino@digital.cabinet-office.gov.uk>
2025-03-26 10:00:12 +0000

    Fix the Javascript import for v5

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,11 +1,9 @@
-//= require govuk/all.js
 //= require gaap-analytics.min.js
 //= require cookies.js
 //= require ./cookie-banner/cookie-banner.js
 //= require ./cookie-settings/cookie-settings.js
 
 document.addEventListener('DOMContentLoaded', function (event) {
-  GOVUKFrontend.initAll()
   GAAP.analytics.eventTracking.init()
   window.GovUkPay.CookieBanner.checkForBannerAndInit()
   window.GovUkPay.CookieSettings.checkForFormAndInit()
